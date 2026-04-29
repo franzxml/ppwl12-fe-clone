9fe5ec3c5eef65f5ca1c0e92677900a2a5afa5fa
franzxml <ignfranzx@gmail.com>
2026-04-29 21:24:07 +0700

    feat: menambah halaman rooms dan route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import Experiences from './pages/Experiences'
 import Homes from './pages/Homes'
+import Rooms from './pages/Rooms'
+import Services from './pages/Services'
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/homes" replace />} />
           <Route path="/homes" element={<Homes />} />
+          <Route path="/experiences" element={<Experiences />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/rooms" element={<Rooms />} />
         </Routes>
         <Footer />
       </div>
